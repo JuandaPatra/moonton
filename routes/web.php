386f278f8364855d3805b2129e866fb0d1a6e3dc
Rahmad Juanda Patra <patrajuanda10@gmail.com>
2022-07-05 20:00:49 +0700

    finish testing boilerplate

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@ use Inertia\Inertia;
 |
 */
 
+Route::get('admin', function(){
+    return 'admin';
+})->middleware('role:admin');
+
+Route::get('user', function(){
+    return 'user';
+})->middleware('role:user');
+
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
