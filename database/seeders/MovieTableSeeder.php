738f73f9aa9395408073d8d90564d527edd8ca3c
Rahmad Juanda Patra <patrajuanda10@gmail.com>
2022-07-13 16:09:44 +0700

    feat:db integration

---
+++ database/seeders/MovieTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Movie;
+
+class MovieTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $movies =[
+            [
+                'name'          => 'The Shawshank Redemption',
+                'slug'          => 'the-shawshank-redemption',
+                'category'      => 'Drama',
+                'video_url'     => 'https://www.youtube.com/watch?v=oBiggLZI2XU',
+                'thumbnail'     => 'https://source.unsplash.com/640x480?drama',
+                'rating'        => 9.2,
+                'is_featured'   => 1
+            ],
+            [
+                'name'          => 'The Godfather',
+                'slug'          => 'the-godfather',
+                'category'      => 'Drama',
+                'video_url'     => 'https://www.youtube.com/watch?v=oBiggLZI2XU',
+                'thumbnail'     => 'https://source.unsplash.com/640x480?drama',
+                'rating'        => 9.4,
+                'is_featured'   => 0
+            ],
+            [
+                'name'          => 'The Godfather: Part II',
+                'slug'          => 'the-godfather-part-ii',
+                'category'      => 'Drama',
+                'video_url'     => 'https://www.youtube.com/watch?v=oBiggLZI2XU',
+                'thumbnail'     => 'https://source.unsplash.com/640x480?drama',
+                'rating'        => 9.4,
+                'is_featured'   => 0
+            ],
+        ];
+
+        Movie::insert($movies);
+    }
+}
